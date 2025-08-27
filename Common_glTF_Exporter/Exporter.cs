0a91759d98540290860c68c4f8cec02c653c1ef9
Mohammad A.I. <121821310+abir314@users.noreply.github.com>
2025-08-27 16:00:38 +0200

    wip

---
+++ Common_glTF_Exporter/Exporter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Autodesk.Revit.DB;
+using Common_glTF_Exporter.Materials;
+using Common_glTF_Exporter.Utils;
+using Common_glTF_Exporter.Core;
+
+namespace Common_glTF_Exporter
+{
+    public class Exporter
+    {
+        public void Export(Document doc)
+        {
+            ExportLog.StartLog();
+            Autodesk.Revit.DB.View view = doc.ActiveView;
+
+            if (view == null || view.GetType().Name != "View3D")
+            {
+                ExportLog.WriteException(new Exception("Wrong View, You must be in a 3D view to export"));
+                return;
+            }
+            var texturePaths = TextureLocation.GetPaths();
+
+            List<Element> elementsInView = Collectors.AllVisibleElementsByView(doc, view);
+
+            if (!doc.IsFamilyDocument && elementsInView.Count == 0)
+            {
+                ExportLog.WriteException(new Exception("There are no valid elements to export in this view"));
+                return;
+            }
+
+            ExportLog.Write($"{elementsInView.Count} elements will be exported");
+
+            GLTFExportContext ctx = new GLTFExportContext(doc, view);
+            CustomExporter exporter = new CustomExporter(doc, ctx);
+            exporter.ShouldStopOnError = false;
+
+            exporter.Export(view);
+            ExportLog.EndLog();
+        }
+    }
+}
---
+++ Common_glTF_Exporter/Preferences.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Autodesk.Revit.DB;
+
+namespace Common_glTF_Exporter
+{
+    public enum CompressionEnum
+    {
+        None,
+        Meshopt,
+        Draco,
+        ZIP,
+    }
+
+    public enum FormatEnum
+    {
+        gltf,
+        glb,
+    }
+
+    public enum MaterialsEnum
+    {
+        textures,
+        materials,
+        nonematerials
+    }
+
+    public static class Preferences
+    {
+        public static MaterialsEnum Materials { get; set; } = MaterialsEnum.nonematerials;
+        public static FormatEnum Format { get; set; } = FormatEnum.glb;
+        public static bool Normals { get; set; } = false;
+        public static bool Levels { get; set; } = false;
+        public static bool Lights { get; set; } = false;
+        public static bool Grids { get; set; } = false;
+        public static bool BatchId { get; set; } = false;
+        public static bool Properties { get; set; } = false;
+        public static bool RelocateTo0 { get; set; } = false;
+        public static bool FlipAxis { get; set; } = true;
+        public static string Units { get; set; } = UnitTypeId.Meters.TypeId.ToString();
+        public static CompressionEnum Compression { get; set; } = CompressionEnum.None;
+        public static string Path { get; set; } = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        public static string FileName { get; set; } = "3dExport";
+        public static int Runs { get; set; } = 0;
+        public static string User { get; set; } = "user01";
+        public static int Release { get; set; } = 0;
+        public static bool IsRFA { get; set; } = false;
+    }
+}
