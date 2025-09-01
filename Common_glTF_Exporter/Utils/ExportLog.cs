528ec43cbec6a69a4f4551d4857537f0356d3519
Mohammad A.I. <121821310+abir314@users.noreply.github.com>
2025-09-01 11:49:05 +0200

    fix

--- Common_glTF_Exporter/Utils/ExportLog.cs
+++ Common_glTF_Exporter/Utils/ExportLog.cs
@@ -6,7 +6,7 @@ namespace Common_glTF_Exporter.Utils
 {
     public class ExportLog
     {
-        private static readonly string logFilePath = Path.Combine(Exporter.exportOptions.TempDirectory, Exporter.exportOptions.TxtFileName);
+        private static string logFilePath => Path.Combine(Exporter.exportOptions.TempDirectory, Exporter.exportOptions.TxtFileName);
 
         public static void StartLog()
         {
